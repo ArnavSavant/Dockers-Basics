5bc50400bdb1f57499e6722ab35680b3f17f46c2
Arnav Savant <savantishu02@gmail.com>
2023-08-06 19:56:48 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+app.get('/home',(req,res) => {
+   return res.json({message:'OK'});
+})
+
+app.listen(process.env.PORT, ()=>{
+   console.log("Server is up");
+})
